2878fd94d063777198f4445104cfe29f58f0ddfd
Lee-Chang-Min <10billionmin@naver.com>
2022-02-16 22:16:36 +0900

    print

--- print.py
+++ print.py
@@ -33,5 +33,35 @@ print('{} {}' .format('one', '2'))
 print('{1} {0}' .format('one', 'two'))
 
 
+# %s
+print()
+print('%10s' % ('nice'))
+print('{:>10}'.format('nice')) 
 
+print('%-10s' % ('nice'))
+print('{:10}'.format('nice')) 
+
+print('------------------')
+print('{:_>10}'.format('nice'))
+ 
+print('{:^10}'.format('nice')) #중앙정렬
+
+print('%.5s' % ('nice')) #5자리 이후에는 절삭
+print('%.5s' % ('pythonstudy'))
+print('{:10.5}'.format('pythonstudy'))#공간은 10개인데 5개 이후로 절삭
+
+# %d
+print('%d %d' %(1,2))
+print('{} {}' .format(1,2))
+
+print('%4d' % (42))
+print('{:4d}' .format(42))
+
+# %f
+print('%1.8f' % (3.21654654654654))
+print('%f' % (3.21654654654654))
+print('{:f}' .format(3.21654654654654))
+
+print('%06.2f' % (3.126565465412))
+print('{:06.2f}' .format(3.126565465412))
 
